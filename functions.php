e969ef19691ce5527c9e37919a4ff0ec61341ce2
inneska-v <36503787+inneska-v@users.noreply.github.com>
2021-03-01 05:59:18 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,6 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_theme' );
 function enqueue_universal_theme() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/essets/css/universal-theme.css', 'style', null, null );
+    wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style', null, null );
 
 }
