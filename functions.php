a303e9a2359454572d5746266c78cf529a262e21
inneska-v <36503787+inneska-v@users.noreply.github.com>
2021-03-02 06:37:15 +0200

    Add files via upload
    Lesson 3

--- functions.php
+++ functions.php
@@ -1,8 +1,32 @@
 <?php
+//   Добавляем расширенные возможности
+if ( ! function_exists( 'universal_theme_setup' ) ) :
 
-add_action( 'wp_enqueue_scripts', 'enqueue_universal_theme' );
+    function universal_theme_setup() {
+        // title-tag
+        add_theme_support( 'title-tag' );
+        // logo
+        add_theme_support( 'custom-logo', [
+            'width'      => 163,
+            'flex-height' => true,
+            'header-text' => 'Universal',
+            'unlink-homepage-logo' => false, // WP 5.5
+        ] );
+        // menu
+        register_nav_menus( [
+            'header_menu' => 'Меню в шапке',
+            'footer_menu' => 'Меню в подвале'
+	    ] );
+
+    }
+endif;
+add_action( 'after_setup_theme', 'universal_theme_setup' );
+
+
+// Подключение стилей и скиптов
 function enqueue_universal_theme() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style', null, null );
+    wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style');
 
 }
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_theme' );
--- header.php
+++ header.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Universal</title>
     <?php wp_head(); ?>
 </head>
-<body>
-    
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header class="header">
+    <div class="container">
+        <div class="header-wrapper">
+            <?php
+                if( has_custom_logo() ){
+                    // логотип есть выводим его
+                   echo get_custom_logo();
+                } else {
+                    echo 'Universal';
+                }
+            
+                wp_nav_menu( [
+                    'theme_location'  => 'header_menu',
+                    'container'       => 'nav',
+                    'container_class' => 'header-nav',
+                    'menu_class'      => 'header-menu', 
+                    'echo'            => true,
+
+                ] );
+                // Поиск
+                get_search_form();
+            ?>
+            
+        </div>
+
+
+    </div>
+</header>
+
